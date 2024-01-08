bd7993e3c4b2ecaa7a632ec0f4af5ddc20b99e7c
Guewenhtbs <kerguegue@gmail.com>
2024-01-08 17:44:17 +0100

    (main): resolution des erreurs de merge

--- GO/main.go
+++ GO/main.go
@@ -32,7 +32,6 @@ type Image struct {
 	Matrix [][]Pixel
 }
 
-
 func mat_voisinage(Im Image) {
 	for x := 0; x < Im.Width; x++ {
 		for y := 0; y < Im.Height; y++ {
@@ -54,28 +53,6 @@ func mat_voisinage(Im Image) {
 
 	}
 }
-
-func main() {
-
-	// test de la classe Pixel
-	p1 := Pixel{2, 3, 4, 78, [2]int{1, 1}, [][]*Pixel{}}
-	fmt.Println(p1)
-
-	// test de la classe Image
-	im := Image{1080, 1920, 1, [][]Pixel{}}
-	fmt.Println(im)
-
-	// test de fonctionnement de append avec les matrices
-	im.Matrix = append(im.Matrix, []Pixel{})
-	im.Matrix[0] = append(im.Matrix[0], p1)
-	fmt.Println(im)
-	p1 = Pixel{120, 120, 120, 120, [2]int{2, 1}, [][]*Pixel{}}
-	im.Matrix[0] = append(im.Matrix[0], p1)
-	fmt.Println(im.Matrix[0][0].Coord)
-
-	// Decode the JPEG data. If reading from file, create a reader with
-	//
-	reader, err := os.Open("CGR.jpg")
 
 func uint32ToUint8(value uint32) uint8 {
 	scaledValue := float64(value) * (255.0 / 4294967295.0)
