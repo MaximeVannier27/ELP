ec172ec2fd923bff815f241117fb6e557e0ffd9a
Guewenhtbs <kerguegue@gmail.com>
2024-01-08 22:38:52 +0100

    (main): résolution du problème de floutage en uint8 + rajout du premier test en souvenir

--- GO/main.go
+++ GO/main.go
@@ -17,10 +17,10 @@ import ( //"encoding/base64"
 )
 
 type Pixel struct {
-	Red      uint8
-	Green    uint8
-	Blue     uint8
-	Alpha    uint8
+	Red      uint32
+	Green    uint32
+	Blue     uint32
+	Alpha    uint32
 	Coord    [2]int // {x,y}
 	Adjacent [][]*Pixel
 }
@@ -65,7 +65,7 @@ func floutage(im_in Image) *image.RGBA {
 	for y_im := 0; y_im < im_in.Height; y_im++ {
 		for x_im := 0; x_im < im_in.Width; x_im++ {
 			pix_in := im_in.Matrix[y_im][x_im]
-			var red_avg, green_avg, blue_avg, alpha_avg, comp uint8 = 0, 0, 0, 0, 0
+			var red_avg, green_avg, blue_avg, alpha_avg, comp uint32 = 0, 0, 0, 0, 0
 			for y_pix := 0; y_pix < 2*r+1; y_pix++ {
 				for x_pix := 0; x_pix < 2*r+1; x_pix++ {
 					if pix_in.Adjacent[y_pix][x_pix] != nil {
@@ -77,9 +77,8 @@ func floutage(im_in Image) *image.RGBA {
 					}
 				}
 			}
-			im_out.Set(x_im, y_im, color.RGBA{red_avg / comp, green_avg / comp, blue_avg / comp, alpha_avg / comp})
+			im_out.Set(x_im, y_im, color.RGBA{uint32ToUint8(red_avg / comp), uint32ToUint8(green_avg / comp), uint32ToUint8(blue_avg / comp), uint32ToUint8(alpha_avg / comp)})
 		}
-		fmt.Println(y_im)
 	}
 	return im_out
 }
@@ -111,7 +110,7 @@ func initImage(addresse_image string) Image {
 		for x := bordures.Min.X; x < bordures.Max.X; x++ {
 
 			r, g, b, a := image.At(x, y).RGBA()
-			p := Pixel{uint32ToUint8(r), uint32ToUint8(g), uint32ToUint8(b), uint32ToUint8(a), [2]int{x, y}, [][]*Pixel{}}
+			p := Pixel{r, g, b, a, [2]int{x, y}, [][]*Pixel{}}
 			tmp = append(tmp, p)
 		}
 		retour.Matrix = append(retour.Matrix, tmp)
@@ -126,6 +125,8 @@ func main() {
 	test := initImage("CGR.jpg")
 	//fmt.Println(test.Matrix)
 	test = mat_voisinage(test)
+	res := floutage(test)
+	fmt.Println(res)
 
 	file, err := os.Create("FLOU.png")
 
@@ -134,7 +135,7 @@ func main() {
 	}
 	defer file.Close()
 
-	err = png.Encode(file, floutage(test))
+	err = png.Encode(file, res)
 	if err != nil {
 		panic(err)
 
