75eec7f2ea43acfa8873e96c60c7e5abe77af068
Guewenhtbs <kerguegue@gmail.com>
2024-01-15 15:43:03 +0100

    (client_server): l'initialisation de l'image se passe bien, il faut juste rajouter la demande du rayon de floutage

--- GO/server.go
+++ GO/server.go
@@ -13,6 +13,44 @@ import (
 	"net"
 )
 
+type Pixel struct {
+	Red   uint32
+	Green uint32
+	Blue  uint32
+	Alpha uint32
+	Coord [2]int // {x,y}
+}
+
+type Image struct {
+	Width  int
+	Height int
+	Radius int
+	Matrix [][]Pixel
+}
+
+func initImage(image image.Image) Image {
+
+	bordures := image.Bounds()
+
+	var rayon int
+	var tmp []Pixel
+
+	fmt.Print("Rayon de floutage: ")
+	fmt.Scanln(&rayon)
+	retour := Image{bordures.Max.X, bordures.Max.Y, rayon, [][]Pixel{}}
+	for y := bordures.Min.Y; y < bordures.Max.Y; y++ {
+		tmp = nil
+		for x := bordures.Min.X; x < bordures.Max.X; x++ {
+
+			r, g, b, a := image.At(x, y).RGBA()
+			p := Pixel{r, g, b, a, [2]int{x, y}}
+			tmp = append(tmp, p)
+		}
+		retour.Matrix = append(retour.Matrix, tmp)
+	}
+	return retour
+}
+
 func handleConnection(conn net.Conn) {
 	// Traitement de la connexion ici
 	fmt.Println("Nouvelle connexion établie!")
@@ -32,7 +70,8 @@ func handleConnection(conn net.Conn) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(image)
+	im := initImage(image)
+	fmt.Println(im)
 }
 
 func main() {
