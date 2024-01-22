ece29c065b7749b5685e16fd4a6fd74d16b2075f
Guewenhtbs <kerguegue@gmail.com>
2024-01-22 14:50:06 +0100

    (API_get): définitions des types

--- ELM/src/main.elm
+++ ELM/src/main.elm
@@ -1,1 +1,16 @@
 module Main exposing (..)
+import Json.Decode exposing (Decoder, map2, field, string)
+
+--type alias Package = 
+    --{ word : string
+    --, meanings : Meanings
+    --}
+
+type alias Meanings =
+    { partOfSpeech : string
+    , definitions : Definitions
+    }
+
+type alias Definitions =
+    { definition : string
+    }
