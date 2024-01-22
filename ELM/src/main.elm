e804acaf6570dc1b9032daf511747b5b2229d7bc
Guewenhtbs <kerguegue@gmail.com>
2024-01-22 15:48:29 +0100

    (API_get): recupération de la biblioteheque avec hhtp

--- ELM/src/main.elm
+++ ELM/src/main.elm
@@ -1,6 +1,17 @@
 module Main exposing (..)
 import Json.Decode exposing (Decoder, map2, field, string)
 
+type State
+    = gotPackage (Result Http.Error String)
+    | gotDef 
+
+getPackage : () -> (State)
+getPackage =
+  Http.get
+    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word
+    , expect = Http.expectString gotPackage packageDecoder
+    }
+
 type alias Package = 
     { word : string
     , meanings : Meanings
@@ -15,16 +26,22 @@ type alias Definitions =
     { definition : string
     }
 
+
+packageDecoder : Decoder Package
 packageDecoder =
     map2 Package
         (field "word" string)
         (field "meanings" meaningsDecoder)
 
+meaningsDecoder : Decoder Meanings
 meaningsDecoder =
     map2 Meanings
         (field "partOfSpeech" string)
         (field "definitions" definitionsDecoder)
 
+definitionsDecoder : Decoder Definitions
 definitionsDecoder =
     field "definition" string
+
+
 
