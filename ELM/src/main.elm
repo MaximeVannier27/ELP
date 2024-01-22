f5c8bd8dea04c67c3955e5b3d1363d8d9e040e4a
Guewenhtbs <kerguegue@gmail.com>
2024-01-22 16:09:42 +0100

    (main): mise en forme

--- ELM/src/main.elm
+++ ELM/src/main.elm
@@ -2,49 +2,6 @@ module Main exposing (..)
 import Json.Decode exposing (Decoder, map2, field, string)
 
 
-type State
-    = gotPackage (Result Http.Error Package)
-    | gotDef 
-
-getPackage : () -> (State)
-getPackage =
-  Http.get
-    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word
-    , expect = Http.expectString gotPackage mainDecoder
-    }
-
-type alias Package = 
-    { word : string
-    , meanings : List Meanings
-    }
-
-type alias Meanings =
-    { partOfSpeech : string
-    , definitions : List Definitions
-    }
-
-type alias Definitions =
-    { definition : string
-    }
-
-mainDecoder = at["0"](packageDecoder)
-
-packageDecoder : Decoder Package
-packageDecoder =
-    map2 Package
-        (field "word" string)
-        (field "meanings" (List meaningsDecoder))
-
-meaningsDecoder : Decoder Meanings
-meaningsDecoder =
-    map2 Meanings
-        (field "partOfSpeech" string)
-        (field "definitions" (List definitionsDecoder))
-
-definitionsDecoder : Decoder Definitions
-definitionsDecoder =
-    field "definition" string
-
 
 
 
@@ -139,6 +96,53 @@ view model =
 
 vERIF_MOT = "test"
 dECOUVERTE_MOT = "test"
+
+-- HTTP
+
+
+type State
+    = gotPackage (Result Http.Error Package)
+    | gotDef 
+
+getPackage : () -> (State)
+getPackage =
+  Http.get
+    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word
+    , expect = Http.expectString gotPackage mainDecoder
+    }
+
+type alias Package = 
+    { word : string
+    , meanings : List Meanings
+    }
+
+type alias Meanings =
+    { partOfSpeech : string
+    , definitions : List Definitions
+    }
+
+type alias Definitions =
+    { definition : string
+    }
+
+mainDecoder = at["0"](packageDecoder)
+
+packageDecoder : Decoder Package
+packageDecoder =
+    map2 Package
+        (field "word" string)
+        (field "meanings" (List meaningsDecoder))
+
+meaningsDecoder : Decoder Meanings
+meaningsDecoder =
+    map2 Meanings
+        (field "partOfSpeech" string)
+        (field "definitions" (List definitionsDecoder))
+
+definitionsDecoder : Decoder Definitions
+definitionsDecoder =
+    field "definition" string
+
 
 
 
